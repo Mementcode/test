82d258ca52ec7e9b80d0ac6fa52f09fd2a7beac6
callum brennan <callum.brennan@hotmail.co.uk>
2017-03-31 14:34:05 +0100

    my first commit

--- Test/ViewController.swift
+++ Test/ViewController.swift
@@ -3,18 +3,42 @@
 //  Test
 //
 //  Created by callum brennan on 30/03/2017.
-//  Copyright © 2017 callum brennan. All rights reserved.
+//  Copyright © 2017 callum brennan. All rights reserved..
 //
 
 import UIKit
 
 class ViewController: UIViewController {
-
+  
+    @IBOutlet weak var yolabel: UILabel!
+    
+    var tapCount = 0
+    
+    @IBAction func thebuttonwaspressed(sender: AnyObject) {
+        
+     tapCount = tapCount + 1
+        print (tapCount)
+        
+        if tapCount >= 10 {
+            yolabel.text = "you tapped the button 10 times"
+            
+        }
+        
+    }
+    
+    @IBAction func button2(sender: AnyObject) {
+        
+        yolabel.text = "buttons are cool"
+        
+    }
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+    
     }
-
+  
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
